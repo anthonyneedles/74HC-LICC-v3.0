eebb9ba67b188e8159a9009d640f3fadd3db6ba8
anthonyneedles <aneedles758@gmail.com>
2019-08-02 22:07:46 -0400

    v3.0 - Empty project generated via CubeMX

---
+++ LICC v3.0/Src/main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+* 	74HHC Logic IC Checker (LICC) v3.0
+*
+* 	MCU: STM32F030C8Tx
+*
+* 	08/02/2019:
+*	Created empty project framework.
+*
+* 	Created on: 08/02/2019
+* 	Author: Anthony Needles
+******************************************************************************/
+#include "stm32f030x8.h"
+
+int main(void)
+{
+  while (1)
+  {
+
+  }
+
+}
